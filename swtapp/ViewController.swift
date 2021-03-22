e03eb84dd23471c904e7363fbd62902270658468
chenhui <gaischen@163.com>
2021-03-22 09:15:41 +0800

    Initial Commit

---
+++ swtapp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swtapp
+//
+//  Created by 陈辉 on 2021/3/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
