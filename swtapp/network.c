c24a4088390723e22ceaffa141b480cd69817f9c
chenhui <gaischen@163.com>
2021-03-29 17:56:19 +0800

    add network

---
+++ swtapp/network.c
@@ -0,0 +1,11 @@
+//
+//  network.c
+//  swtapp
+//
+//  Created by 陈辉 on 2021/3/29.
+//  解决客户端网络探测相关的
+//
+
+#include "network.h"
+
+
---
+++ swtapp/network.h
@@ -0,0 +1,13 @@
+//
+//  network.h
+//  swtapp
+//
+//  Created by 陈辉 on 2021/3/29.
+//
+
+#ifndef network_h
+#define network_h
+
+#include <stdio.h>
+
+#endif /* network_h */
